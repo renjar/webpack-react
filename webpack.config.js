10e07c11c728ecaf3a84fdfe284147ec1ed6739f
renjar <renjar.cn@gmail.com>
2016-10-29 16:41:56 +0800

    add some comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
